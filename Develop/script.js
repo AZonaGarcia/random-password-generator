c4d04eee3722d0f58d4e008e130971138e65a2b0
Bonus Mom <AZonaGarcia@aol.com>
2020-08-01 23:54:54 -0400

    added characters to avaliable character array

--- Develop/script.js
+++ Develop/script.js
@@ -12,17 +12,37 @@ function writePassword() {
   var upperLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
   var numerals = "0123456789".split("")
   var specChar = "!@#$%^&*".split("")
-
- 
-
 }
 
 function generatePassword() {
+  var availableChar = []
+  var passwordArr = []
+  
   var lowerLetter = confirm("Would you like to use lower letters?");
   var upperLetter = confirm("Would you like to use upper letters?");
   var numerals = confirm("Would you like to use numbers?");
   var specChar = confirm("Would you like to use special characters?");
-  var passCharCount = prompt("Choose a password length between 8 and 128");
+  var passCharCount = prompt("Choose a password length between 8 and 128")
+  
+  if (lowerLetter === true) {
+    availableChar.push(lowerLetter)
+  }
+  if (upperLetter === true) {
+    availableChar.push(upperLetter)
+  }
+  if (numerals === true) {
+    availableChar.push(numerals)
+  }
+  if (specChar === true) {
+    availableChar.push(specChar)
+  }
+
+  for (var i = 0; i < passCharCount; i++) {
+    passwordArr = Math.floor(Math.random) * availableChar
+    passwordArr.push(passCharCount)
+    console.log(availableChar)
+  }
+
   return password;  
 }
 
